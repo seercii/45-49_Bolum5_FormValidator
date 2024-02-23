94bb22474614be7685b7919fb7c7fd35754a5e1e
Sercan Güre <sercangure@yahoo.com>
2024-02-23 12:45:10 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const form= document.querySelector("#form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const repassword = document.getElementById("repassword");
+
+
+function error(input,message){
+    input.className='form-control is-invalid'
+    const div = input.nextElementSibling;
+    div.innerText=message;
+    div.className='invalid-feedback';
+}
+
+function success(input){
+    input.className='form-control is-valid'
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    
+    if(username.value===''){
+        error(username,"Kullanıcı adını giriniz")
+    }else{
+        success(username);
+    }
+
+    if(email.value===''){
+        error(email,"Mail adresinizi giriniz")
+    }else if(!validateEmail(email.value)){
+        error(email,'düzgün bir mail giriniz')
+    }   
+    else{
+        success(email);
+    }
+
+    if(password.value===''){
+        error(password,"Şifrenizi giriniz")
+    }else{
+        success(password);
+    }
+
+    if(repassword.value===''){
+        error(repassword,"Şifrenizi tekrar giriniz")
+    }else{
+        success(repassword);
+    }
+
+});
